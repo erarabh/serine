0e2e75c56ac4382d7809a3117bbf4797298cf6c3
erarabh <rabhirabah2015.com>
2025-06-06 21:54:53 +0100

    update PricingCard.tsx

--- components/PricingCard.tsx
+++ components/PricingCard.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 interface Props {
   title: string
   price: string
+  popular?: boolean; // ← Add this line
   features: string[]
   buttonLabel: string
   buttonHref?: string
